e0d8b7482ff3601f1e7a97aef945fdc397bc3199
maxhormazabal <mhormazabal.lagos@gmail.com>
2023-08-09 15:22:41 -0400

    Foto actualizada dr pereira

--- js/estudios_lightbox.js
+++ js/estudios_lightbox.js
@@ -1,26 +1,9 @@
-// Función para abrir la Lightbox y cargar el video
-function openLightbox(videoURL) {
-  const videoIframe = document.getElementById("videoIframe");
-  videoIframe.src = videoURL;
-
-  const lightbox = document.getElementById("videoLightbox");
-  lightbox.style.display = "block";
-}
-
-// Función para cerrar la Lightbox y detener el video
-function closeLightbox() {
-  const videoIframe = document.getElementById("videoIframe");
-  videoIframe.src = "";
-
-  const lightbox = document.getElementById("videoLightbox");
-  lightbox.style.display = "none";
-}
-
-// Evento para abrir la Lightbox al hacer clic en el botón de reproducción
-const playButtons = document.querySelectorAll(".btn-play");
-playButtons.forEach((button) => {
-  button.addEventListener("click", function() {
-    const videoURL = button.getAttribute("data-video-link");
-    openLightbox(videoURL);
+$(document).ready(function() {
+  // Inicializar el Lightbox con las opciones deseadas (puedes personalizarlo según tus necesidades)
+  $("[data-fancybox]").fancybox({
+    youtube: {
+      controls: 1,
+      showinfo: 0,
+    },
   });
 });
